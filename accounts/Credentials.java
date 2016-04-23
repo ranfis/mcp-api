6f8bd9721148c7c050e33e53831d9bb54541eb92
Ranfis S <ranfis.sh@gmail.com>
2016-04-23 19:17:01 -0400

    Update credentials class to support login via token

--- accounts/Credentials.java
+++ accounts/Credentials.java
@@ -3,11 +3,21 @@ package com.mcp.mycareerplan.api.accounts;
 public class Credentials{
     public String Usuario;
     public String clave;
+    public String token;
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
     public Credentials(String Usuario, String clave) {
         this.Usuario = Usuario;
         this.clave = clave;
     }
-
+    public Credentials(String token) {
+        this.token = token;
+    }
     public String getUsuario() {
         return Usuario;
     }
